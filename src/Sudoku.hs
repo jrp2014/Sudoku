1acff7437cf9cf234c1ab09d8fbdf3ed712a22c6
jrp2014 <jrp@dial.pipex.com>
2019-05-22 21:18:59 +0100

    First complete attempt

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -5,10 +5,6 @@
 module Sudoku where
 
 import Control.Applicative
-import Control.Monad
-import Data.Char
-import Data.Foldable
-import Data.Monoid
 import Data.Traversable
 import NanoParsec
 
@@ -26,12 +22,6 @@ pattern Tr a b c = (Id a :*: Id b) :*: Id c
 
 type Row = Triple :. Triple
 
-zone :: Row Char
-zone = O (Tr (Tr 'a' 'b' 'c') (Tr 'd' 'e' 'f') (Tr 'g' 'h' 'a'))
-
-zone2 :: Row [Value]
-zone2 = O (Tr (Tr "ab" "bc" "c") (Tr "d" "e" "f") (Tr "g" "h" "ab"))
-
 type Grid = Matrix Value
 
 type Matrix = Row :. Row
@@ -43,9 +33,9 @@ type Choices = [Value]
 showValue :: Value -> Char
 showValue = id
 
-showCell :: Choices -> String
---showCell vs = '[' : vs ++ replicate (9 - length vs) ' ' ++ "]"
-showCell xs =
+showChoices :: Choices -> String
+--showChoices vs = '[' : vs ++ replicate (9 - length vs) ' ' ++ "]"
+showChoices xs =
   (++ "]") .
   Data.List.foldl'
     (\acc x ->
@@ -56,17 +46,17 @@ showCell xs =
     "[" $
   ['1' .. '9']
 
-showTriple :: Triple [Value] -> String
-showTriple (Tr a b c) = showCell a ++ showCell b ++ showCell c
+showTriple :: Triple Choices -> String
+showTriple (Tr a b c) = showChoices a ++ showChoices b ++ showChoices c
 
 showRow :: Row Choices -> String
 showRow (O (Tr a b c)) = showTriple a ++ showTriple b ++ showTriple c ++ "\n"
 
 showMatrix :: Matrix Choices -> String
-showMatrix = foldMap showCell
+showMatrix = foldMap showRow . unO
 
 showMatrix' :: Matrix Choices -> String
-showMatrix' (O rs) = foldMap showRow rs
+showMatrix' = foldMap showChoices
 
 -- putStrLn $ unlines $ fmap showRow $ sequenceA $ (fmap.fmap) (:[]) zone2
 --
@@ -87,6 +77,7 @@ rows :: Matrix a -> Matrix a
 rows = id
 
 cols :: Matrix a -> Matrix a
+-- cols = newly sequenceA
 cols = O . sequenceA . unO
 
 boxs :: Matrix a -> Matrix a
@@ -100,32 +91,8 @@ pboard = sequenceA (pure pcell)
 pcell :: Parser Value
 pcell = spaces *> (digit <|> (char '.' $> '0')) <* spaces
 
--- TODO: Put this into the test suite
-tryThis :: String
-tryThis =
-  unlines
-    [ "...23.6.."
-    , "1.......7"
-    , ".4...518."
-    , "5.....9.."
-    , "..73.68.."
-    , "..4.....5"
-    , ".867...5."
-    , "4.......9"
-    , "..3.62..."
-    ]
-
-test1 :: [Value]
-test1 = flatten . fst . head $ parse pboard tryThis
-
-test2 :: Bool
-test2 = safe . fst . head $ parse pboard tryThis
-
-test3 = showMatrix . choices . fst . head $ parse pboard tryThis
-
-test4 =
-  putStrLn . showMatrix' . prune . prune . choices . fst . head $
-  parse pboard tryThis
+parseBoard :: String -> Grid
+parseBoard = fst . head . parse pboard
 
 -- Newtype coercion
 newly :: (g1 (f1 a1) -> g2 (f2 a2)) -> (:.) g1 f1 a1 -> (:.) g2 f2 a2
@@ -145,9 +112,12 @@ reduce = crush id
 flatten :: (Traversable f) => f a -> [a]
 flatten = crush (: [])
 
+valid :: Grid -> Bool
+valid g = all (\f -> nodups $ f g) [rows, cols, boxs]
+
 -- TODO: is this the best we can do?
-nodups :: (Eq a, Foldable f) => f a -> Bool
-nodups l = foldr test end l []
+duplicates' :: (Eq a, Foldable f) => f a -> Bool
+duplicates' l = foldr test end l []
   where
     test :: Eq a => a -> ([a] -> Bool) -> [a] -> Bool
     test a cont seen = (a `elem` seen) || cont (a : seen)
@@ -161,6 +131,9 @@ duplicates s = reduce $ snd $ mapAccumL gather [] s
       if current `elem` seen
         then (seen, [current])
         else (current : seen, [])
+
+nodups :: (Traversable f, Eq a) => f a -> Bool
+nodups = null . duplicates
 
 -- A basic solver
 choices :: Grid -> Matrix Choices
@@ -188,16 +161,15 @@ prune = pruneBy boxs . pruneBy cols . pruneBy rows
     pruneBy f = f . newly (fmap thin) . f
 
 thin :: Row Choices -> Row Choices
-thin xss = fmap (`minus` singles) xss
-  where
-    singles :: Choices
-    singles =
-      foldMap
-        (\a ->
-           if single a
-             then a
-             else mempty)
-        xss
+thin xss = fmap (`minus` singles xss) xss
+
+singles :: Row Choices -> Choices
+singles =
+  foldMap
+    (\a ->
+       if single a
+         then a
+         else mempty)
 
 minus :: Choices -> Choices -> Choices
 xs `minus` ys =
@@ -222,11 +194,14 @@ complete = all single
 void :: Matrix Choices -> Bool
 void = any null
 
-safe :: Eq a => Matrix a -> Bool
-safe t = all (\f -> nodups $ f t) [rows, cols, boxs]
+safe :: Matrix Choices -> Bool
+safe t = consistent (rows t) && consistent (cols t) && consistent (boxs t)
 
-valid :: Grid -> Bool
-valid = safe
+consistent :: Matrix Choices -> Bool
+consistent = and . fmap consistentRow . unO
+
+consistentRow :: Row Choices -> Bool
+consistentRow = nodups . singles
 
 blocked :: Matrix Choices -> Bool
 blocked m = Sudoku.void m || not (safe m)
@@ -241,10 +216,67 @@ search m
   | complete m = collapse m
   | otherwise = [g | m' <- expand m, g <- search (prune m')]
 
-expand = undefined
+-- TODO: This doesn't work
+expand :: Matrix Choices -> [Matrix Choices]
+expand = traverse $ fmap (: [])
 
 counts :: Row Choices -> Row Int
 counts = fmap length
 
 n :: Row Int -> Int
 n = minimum
+
+zone :: Row Char
+zone = O (Tr (Tr 'a' 'b' 'c') (Tr 'd' 'e' 'f') (Tr 'g' 'h' 'a'))
+
+zone2 :: Row [Value]
+zone2 = O (Tr (Tr "ab" "bc" "c") (Tr "d" "e" "f") (Tr "g" "h" "ab"))
+
+-- TODO: Put this into the test suite
+tryThis :: String
+tryThis =
+  unlines
+    [ "...23.6.."
+    , "1.......7"
+    , ".4...518."
+    , "5.....9.."
+    , "..73.68.."
+    , "..4.....5"
+    , ".867...5."
+    , "4.......9"
+    , "..3.62..."
+    ]
+
+easy :: String
+easy =
+  unlines
+    [ "2....1.38"
+    , "........5"
+    , ".7...6..."
+    , ".......13"
+    , ".981..257"
+    , "31....8.."
+    , "9..8...2."
+    , ".5..69784"
+    , "4..25...."
+    ]
+
+test0 :: Grid
+test0 = parseBoard easy
+
+test1 :: [Value]
+test1 = flatten test0
+
+test2 :: Bool
+test2 = valid test0
+
+test3 :: String
+test3 = showMatrix $ choices test0
+
+test4 :: Matrix Choices
+test4 = prune . prune $ choices test0
+
+test5 :: IO ()
+test5 = putStrLn . showMatrix . prune . prune $ choices test0
+
+test6 = length (expand test4)
